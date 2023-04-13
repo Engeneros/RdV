f78266b21ca548d15168dd2e5487a48e68f6d9d3
Engeneros <D.Chernovsky@gmail.com>
2023-04-13 19:20:25 +0700

    ReadV.c

--- ReadV/ReadV.c
+++ ReadV/ReadV.c
@@ -5,9 +5,21 @@
 int ReadV(char* str)
 {
     int pos;
+	int ret;
+	char symb = 'x';
     FILE* fp = fopen("/proc/version", r);
     if(!fp)
     {
-	printf(stderr, "oops, cannot to open\n");
-	
+		printf(stderr, "oops, cannot to open\n");
+		ret = 1;
     }
+	else
+	{
+		for (pos = 0; (pos < STR_SIZE) && symb != EOF); ++pos)
+			str[pos] = symb = fgets(fp);
+		str[pos] = '\0';
+		fclose(fp);
+		ret = 0;
+	}
+	rerurn ret;
+}
